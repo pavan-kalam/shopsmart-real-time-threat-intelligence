a607500ec8b110c327f2e904842827171646f59e
pavan-kalam <kalampavan369@gmail.com>
2025-03-02 08:52:26 -0600

    Connect flask with user table

---
+++ api/models.py
@@ -0,0 +1,12 @@
+# src/api/models.py
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
