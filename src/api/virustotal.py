7bd1915726d0fa59d68c65b601eba60232e90cf4
pavan-kalam <kalampavan369@gmail.com>
2025-03-28 15:51:53 -0500

    modifications

--- src/api/virustotal.py
+++
@@ -1,9 +0,0 @@
-# api/virustotal.py
-import requests
-
-def fetch_virustotal_data(api_key, url):
-    headers = {
-        'x-apikey': api_key
-    }
-    response = requests.get(f'https://www.virustotal.com/api/v3/urls/{url}', headers=headers)
-    return response.json()
