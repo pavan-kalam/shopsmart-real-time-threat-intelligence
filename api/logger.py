cc5437e79695e8ee8f16ef7e79d5232f35c6e1d7
pavan-kalam <kalampavan369@gmail.com>
2025-03-28 15:19:33 -0500

    logger information

--- api/logger.py
+++ api/logger.py
@@ -1,13 +1,23 @@
+# api/logger.py
 import logging
+import os
+from dotenv import load_dotenv
 
-def setup_logger():
-    """Set up the logger for the application."""
-    logger = logging.getLogger('osint_fetcher')
-    logger.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    return logger
+load_dotenv()
 
-logger = setup_logger()
+# Configure the log file path from environment variable, default to 'app.log'
+LOG_FILE_PATH = os.getenv('LOG_FILE_PATH', 'app.log')
+
+# Configure the logger for the application
+logger = logging.getLogger('app')
+logger.setLevel(logging.INFO)
+
+# File handler
+file_handler = logging.FileHandler(LOG_FILE_PATH)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(file_handler)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(console_handler)
